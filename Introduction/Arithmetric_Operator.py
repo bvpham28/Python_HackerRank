90371a643a8cad66d3c3c4e3035486f303529900
Bryan Pham <Phamtasticc@gmail.com>
2017-05-29 15:36:43 -0700

    Python HackerRank: Arithmetric Operator

---
+++ Introduction/Arithmetric_Operator.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    addition = a + b
+    difference = a - b
+    times = a * b
+
+print(addition)
+print(difference)
+print(times)
