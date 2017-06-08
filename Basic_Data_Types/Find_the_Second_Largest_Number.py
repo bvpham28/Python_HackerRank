6a10beea6df0a5da393bdf609d807721ffed5cf3
Bryan Pham <Phamtasticc@gmail.com>
2017-06-08 14:49:17 -0700

    Python HackerRank: Find the Second Largest Number

---
+++ Basic_Data_Types/Find_the_Second_Largest_Number.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    i = int(input())
+    lis = list(map(int, input().strip().split()))[:i]
+    z = max(lis)
+    while max(lis) == z:
+        lis.remove(max(lis))
+
+    print (max(lis))
+
